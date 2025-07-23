a8d85abb3d612892cebe4eaac04693e502256654
Victor-V21 <vegav2605@gmail.com>
2025-07-23 09:25:51 -0600

    Add db linux connection

--- MiCampus/obj/Debug/net9.0/MiCampus.AssemblyInfo.cs
+++ MiCampus/obj/Debug/net9.0/MiCampus.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MiCampus")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90e1b091da3ea6a6d493b4c8f1c1606a20fcdb92")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f56f7f85550d331486ddd65e59d7529b549153c7")]
 [assembly: System.Reflection.AssemblyProductAttribute("MiCampus")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MiCampus")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
